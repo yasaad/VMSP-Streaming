36f1e6733eda2c6b723a08d9518578692a939faa
Youssef Asaad <youssef.a.asaad@gmail.com>
2021-10-09 19:29:44 -0400

    added timer functionality

--- ATEM Streamer/src/main/python/main.py
+++ ATEM Streamer/src/main/python/main.py
@@ -4,9 +4,9 @@ import calendar
 from argparse import ArgumentParser
 from datetime import datetime, date
 from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtWidgets import QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QComboBox, QLineEdit
-from PyQt5.QtGui import QIcon, QPixmap, QMovie, QPainter
-from PyQt5.QtCore import QObject, pyqtSignal, QRunnable, QThreadPool, pyqtSlot
+from PyQt5.QtWidgets import QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QComboBox, QLineEdit, QSizePolicy
+from PyQt5.QtGui import QIcon, QPixmap, QMovie, QPainter, QFont
+from PyQt5.QtCore import QObject, pyqtSignal, QRunnable, QThreadPool, pyqtSlot, QTimer, Qt
 from streamAutomation import StreamAutomation
 
 
@@ -116,18 +116,29 @@ class Window(QWidget):
         super().__init__()
         #TODO: Parse arguments from automation parameters
         self.parser = ArgumentParser()
-        self.parser.add_argument('--title', help='Title and Thumbnail')
-        self.parser.add_argument('--duration', help='Time in hours')
+        self.parser.add_argument('-t', '--title', type=str, help='''Title and Thumbnail options:
+                                                        Divine Liturgy
+                                                        Vespers & Midnight Praises
+                                                        Bible Study
+                                                        Palm Sunday
+                                                        Holy Week
+                                                        Coptic New Year
+                                                        Feast of Nativity
+                                                        Feast of Resurrection
+                                                        Feast of the Cross
+                                                        Feast of Theophany
+                                                        Virgin Mary Revival''')
+        self.parser.add_argument('-d', '--duration', type=float, help='Time in hours')
+        self.parser.add_argument('-u', '--unlisted', help='Use to make stream unlisted', action='store_true')
+        self.parser.add_argument('-a', '--autostart', help='Use to autostart stream', action='store_true')
         self.args = self.parser.parse_args()
         self.ctx = ctx
         self.threadpool = QThreadPool()
         self.streamAutomation = StreamAutomation(self.ctx.get_resource("keys"))
         self.thumbnail_path = None
         self.broadcast_id = None
-        self.styleSheetBlack = (
-            'background-color:black'
-        )
-        self.styleSheetWhite = ('background-color:white')
+        self.font= QFont("Arial", 16)
+        self.styleSheetWhite = ('background-color:white; padding-top: 3px; padding-bottom: 3px; padding-left: auto; padding-right: auto;')
         #maps title to image
         self.titlesDict = {
             'Divine Liturgy':'Divine_Liturgy.jpg',
@@ -158,33 +169,74 @@ class Window(QWidget):
         header = QHBoxLayout()
         header.addWidget(self.titleSelector)
         titleLayout = QHBoxLayout()
-        titleLayout.addWidget(QLabel("Video Title:"))
+        video_title = QLabel("Video Title:")
+        video_title.setFont(self.font)
+        titleLayout.addWidget(video_title)
         titleLayout.addWidget(self.title)
         footer = QHBoxLayout()
         footer.addWidget(self.visibilitySelector)
         footer.addWidget(self.startStreamBtn)
+        timerLayout = QHBoxLayout()
+        timerLayout.addWidget(self.resetBtn)
+        timerLayout.addWidget(self.timer_label)
         self.mainVbox.addLayout(header)
         self.mainVbox.addWidget(self.thumbnail)
         self.mainVbox.addLayout(titleLayout)
         self.mainVbox.addLayout(footer)
+        self.mainVbox.addLayout(timerLayout)
 
     def create_widgets(self):
         self.titleSelector = QComboBox()
+        self.titleSelector.setFont(self.font)
         self.titleSelector.addItems([*self.titlesDict])
         self.titleSelector.setCurrentIndex(0)
+        # Set Title Selector if argument given
         if self.args.title in self.titlesDict:
             index = self.titleSelector.findText(self.args.title)
             self.titleSelector.setCurrentIndex(index)
+            
+            
         self.titleSelector.currentIndexChanged.connect(self.title_selector_changed)
         self.title = QLineEdit(self.create_title(self.titleSelector.currentText()))
+        self.title.setFont(self.font)
+        
+        # Set duration
+        self.remaining_time = -1
+        self.timer_label = QLabel()
+        if self.args.duration and float(self.args.duration) > 0:
+            self.remaining_time = float(self.args.duration)
+            self.update_timer()
+        else:
+            self.timer_label.setText("No timer set")
+        self.timer_label.setFont(self.font)
+        # self.timer_label.setAlignment(Qt.AlignCenter)
+        self.timer_label.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+        self.timer_label.adjustSize()
         self.visibilitySelector = QComboBox()
         self.visibilitySelector.addItems(["Public", "Unlisted"])
+        self.visibilitySelector.setFont(self.font)
+        if self.args.unlisted:
+            self.visibilitySelector.setCurrentIndex(1)
         self.startStreamBtn = LoadingButton("Start Stream")
+        self.startStreamBtn.setFont(self.font)
         self.startStreamBtn.setGif(self.ctx.get_resource('images/loading.gif'))
         self.startStreamBtn.setStyleSheet(self.styleSheetWhite)
         self.startStreamBtn.clicked.connect(lambda: self.stop_stream() if self.streaming else self.start_stream())
+        self.resetBtn = QPushButton("Reset Timer")
+        self.resetBtn.setFont(self.font)
+        self.resetBtn.clicked.connect(self.reset_clicked)
+        self.resetBtn.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+        self.resetBtn.setStyleSheet(self.styleSheetWhite)
         self.thumbnail = Thumbnail(self)
         self.thumbnail_path = self.set_thumbnail(self.titlesDict[self.titleSelector.currentText()])
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer_interval)
+        
+        if self.args.autostart:
+            self.start_stream()
+  
+
+            
 
     def create_title(self, selection):
         title = selection
@@ -217,11 +269,43 @@ class Window(QWidget):
 
     def thread_complete(self):
         self.startStreamBtn.stop()
+        
         self.startStreamBtn.setText("Stop Stream" if self.streaming else "Start Stream")
         self.startStreamBtn.setDisabled(False)
     
+    def reset_clicked(self):
+        if self.timer.isActive():
+            self.timer.stop()
+            self.timer_label.setText("Timer Canceled")
+            self.resetBtn.setText("Reset Timer")
+            return
+        self.remaining_time = 2 * 3600
+        self.update_timer()
+        
+        
+    def timer_interval(self):
+        if self.remaining_time == 0:
+            self.timer.stop()
+            self.stop_stream()
+            self.timer_label.setText("AutoStop Timer: Stream Completed")
+            return
+        self.remaining_time -= 1
+        self.update_timer()
+        
+    def update_timer(self):
+        
+        hours = self.remaining_time // 3600
+        remaining = self.remaining_time % 3600
+        minutes = remaining // 60
+        seconds = remaining % 60
+        self.timer_label.setText(f"Time Remaining: {hours}:{minutes:02d}:{seconds:02d}")
+        self.timer_label.adjustSize()
+    
     def start_stream(self):
         self.streaming = True
+        if self.remaining_time > 0:
+            self.timer.start(1000)
+            self.resetBtn.setText('Cancel Timer')
         self.startStreamBtn.setDisabled(True)
         self.startStreamBtn.start()
         worker = Worker(start_stream_process, self.streamAutomation, self.title.text(), self.visibilitySelector.currentText(), self.thumbnail_path)
@@ -232,6 +316,9 @@ class Window(QWidget):
 
     def stop_stream(self):
         self.streaming = False
+        if self.timer.isActive():
+            self.timer.stop()
+            self.resetBtn.setText('Reset Timer')
         self.startStreamBtn.setDisabled(True)
         self.startStreamBtn.start()
         worker = Worker(stop_stream_process, self.streamAutomation, self.broadcast_id)
--- ATEM Streamer/src/main/python/taskScheduler.py
+++ ATEM Streamer/src/main/python/taskScheduler.py
@@ -1,48 +1,101 @@
 import datetime
 import win32com.client
+from enum import Enum
+import win_task
 
-scheduler = win32com.client.Dispatch('Schedule.Service')
-scheduler.Connect()
-root_folder = scheduler.GetFolder('\Streaming')
-task_def = scheduler.NewTask(0)
 
-# Defining the Start time of job
+### TODO: UNDERSTAND WEATHER OR NOT I WANT TO RUN WITH HIGHEST PRIVILEGE IN LINE 1058 of win_task.py
+
+class Scheduler():
+
+    class Trigger_frequency(Enum):
+        ONE_TIME = 1
+        DAILY = 2
+        WEEKLY = 3
+
+
+    def __init__(self):
+        self.scheduler = win32com.client.Dispatch('Schedule.Service')
+        self.scheduler.Connect()
+        self.root_folder = self.scheduler.GetFolder('\Streaming')
+
+    def print_tasks(self, XML=False):
+        for task in self.root_folder.GetTasks(0):
+            print(f'Name: "{task.Name}", Previous Runtime: {task.LastRunTime}, Next Runtime: {task.NextRunTime}')
+            if XML:
+                print(task.XML)
+    
+    def test_task_def(self, task_def):
+        # Register Task
+        # If task already exists, it will be updated
+        TASK_CREATE_OR_UPDATE = 6
+        # 0 for run when user is logged on and 1 for run when user is logged on or not
+        TASK_LOGON_NONE = 0 
+        self.root_folder.RegisterTaskDefinition(
+            'Test Task', #Task Name
+            task_def,
+            TASK_CREATE_OR_UPDATE,
+            'strea', #user
+            'Jesusislord@33', #password
+            TASK_LOGON_NONE
+        )
+    def create_new_task(self, name: str, start_time, action_path: str, action_arguments: str, trigger_frequency: Trigger_frequency):
+
+        task_def = self.scheduler.NewTask(0)
+        # trigger = task_def.Triggers.Create(0)
+
+        #Repeat for a duration of number of days between
+        # num_of_days = 10
+        # trigger.Repetition.Duration = "P"+str(num_of_days)+"D"
+
+        #use PT2M for every 2 minutes, use PT1H for every 1 hours
+        # trigger.repetition.Interval = "PT2M"
+        # trigger.StartBoundary = start_time.isoformat()
+
+        # Create Action
+        TASK_ACTION_EXEC = 0 
+        action = task_def.Actions.Create(TASK_ACTION_EXEC)
+        # action.ID = "TRIGGER BATCH"
+        action.path = action_path
+        action.Arguments = action_arguments
+
+        # Set Parameters
+        # task_def.RegistrationInfo.Description = "Test Task Description"
+        # task_def.Settings.Enabled = True
+        # task_def.Settings.StopIfGoingOnBatteries = False
+
+        # Register Task
+        # If task already exists, it will be updated
+        TASK_CREATE_OR_UPDATE = 6
+        # 0 for run when user is logged on and 1 for run when user is logged on or not
+        TASK_LOGON_NONE = 0 
+        self.root_folder.RegisterTaskDefinition(
+            name, #Task Name
+            task_def,
+            TASK_CREATE_OR_UPDATE,
+            'strea', #user
+            'Jesusislord@33', #password
+            TASK_LOGON_NONE
+        )
+LOCATION = '\Streaming'
+USERNAME = 'strea'
+PASSWORD = 'Jesusislord@33'
+scheduler = Scheduler()
+print(win_task.create_task(name="WIN_TASK_TEST9",
+                           location=LOCATION,
+                           cmd='echo',
+                           user_name=USERNAME, 
+                           password=PASSWORD, 
+                           force=True,
+                        #    force=True, cmd='echo "Hello"', 
+                        #    description="Auto Generated Task Description", 
+                        #    enabled=True,
+                        #    trigger_type= "OnTaskCreation"
+                           ))
+
+# scheduler.print_tasks(XML=False)
 start_time = datetime.datetime.now() + datetime.timedelta(minutes=1)
-
-#For Daily Trigger set this variable to 2; for One time run set this value as 1
-TASK_TRIGGER = 1
-trigger = task_def.Triggers.Create(TASK_TRIGGER)
-
-#Repeat for a duration of number of days between
-num_of_days = 10
-# trigger.Repetition.Duration = "P"+str(num_of_days)+"D"
-
-#use PT2M for every 2 minutes, use PT1H for every 1 hours
-# trigger.repetition.Interval = "PT2M"
-trigger.StartBoundary = start_time.isoformat()
-
-# Create Action
-TASK_ACTION_EXEC = 0 
-action = task_def.Actions.Create(TASK_ACTION_EXEC)
-action.ID = "TRIGGER BATCH"
-action.path = "C:\\Users\\strea\\AppData\\Local\\ATEM Streamer\\ATEM Streamer.exe"
-action.Arguments = ""
-
-# Set Parameters
-task_def.RegistrationInfo.Description = "Test Task Description"
-task_def.Settings.Enabled = True
-task_def.Settings.StopIfGoingOnBatteries = False
-
-# Register Task
-# If task already exists, it will be updated
-TASK_CREATE_OR_UPDATE = 6
-# 0 for run when user is logged on and 1 for run when user is logged on or not
-TASK_LOGON_NONE = 0 
-root_folder.RegisterTaskDefinition(
-    'Test Task', #Task Name
-    task_def,
-    TASK_CREATE_OR_UPDATE,
-    'strea', #user
-    'Jesusislord@33', #password
-    TASK_LOGON_NONE
-)
+action_path = "C:\\Users\\strea\\AppData\\Local\\ATEM Streamer\\ATEM Streamer.exe"
+action_arguments = ""
+scheduler.create_new_task("TEST2", start_time, action_path, action_arguments, scheduler.Trigger_frequency.DAILY)
+print(win_task.list_tasks(LOCATION))
